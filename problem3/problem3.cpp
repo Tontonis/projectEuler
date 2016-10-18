79972a6ffae09cf4380ccdc6aa9df42b497373ef
Tontonis <hugoaday@gmail.com>
2016-10-18 22:38:11 +0100

    problems 1-3

---
+++ problem3/problem3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	long long int x;
+	x = 0;
+	while(x<=0) {
+		cout << "Enter an integer greater than 0." << endl;
+	  cin >> x;
+	}
+
+	for(int k=2; k<(sqrt((double)x)); k++){
+		if(x % k == 0) {
+			x = x/k;
+		}
+	}
+
+	int count = 0;
+	int temp = 0;
+	int largestprime = 0;
+	for(long long int i=(x/2); i>0; i--){
+		if(x % i== 0) {
+			count = 0;
+			for(long long int j=(i/2); j>0; j--){
+				if(i % j== 0) {
+		          count++;
+				}
+			}
+			if(count == 1 && largestprime == 0) largestprime= i;
+		}
+	}
+	if (largestprime != 0) {
+	cout << largestprime << " is the largest prime multiple of " << x << endl;;
+	}
+	else {
+		cout << x << " is a prime number." << endl;
+	}
+
+
+	return 0;
+}
